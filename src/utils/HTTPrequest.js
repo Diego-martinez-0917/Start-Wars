4463916667cdecc48d0b69307d376732d7ec7211
Diego-Martinez <d.ricardo2martinez@gmail.com>
2021-03-06 11:49:32 -0500

    change url

--- src/utils/HTTPrequest.js
+++ src/utils/HTTPrequest.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const baseUrl = 'https://swapi.dev/api/';
 export const getPeople = () => {
   try {
-    return getPeopleData(`${baseUrl}people`);
+    return getPeopleData(`${baseUrl}people/`);
   } catch (error) {
     throw error;
   }
@@ -16,7 +16,9 @@ async function getPeopleData(url) {
       method: 'GET',
     });
     result = result.concat(response.data.results);
-    url = response.data.next;
+    url = response.data.next
+      ? response.data.next.replace('http', 'https')
+      : null;
   }
   return result;
 }
